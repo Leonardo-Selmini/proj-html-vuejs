3baa6ab6052c0013ea515739392eb54a67761e1d
Leonardo.Selmini <selminileonardo@gmail.com>
2022-01-12 12:04:28 +0100

    Main - Intro Section

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,115 @@
 <template>
   <div id="app">
-    <Header/>
-    <Main/>
-    <Footer/>
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 // 00000000 SCRIPT
 <script>
-  // import
-  import Header from './components/macro/Header.vue';
-  import Main from './components/macro/Main.vue';
-  import Footer from './components/macro/Footer.vue';
+// import
+import Header from "./components/macro/Header.vue";
+import Main from "./components/macro/Main.vue";
+import Footer from "./components/macro/Footer.vue";
 
-  // export
-  export default {
-    name: 'App',
-    components: {
-      Header,
-      Main,
-      Footer
-    }
-  }
+// export
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+};
 </script>
 
 // 000000000 STYLE 
 <style lang="scss">
-@import './assets/global/global-variables.scss';
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;900&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap');
+@import "./assets/global/global-variables.scss";
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap");
 
-  // commons
-  * {
-    box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
+// commons
+* {
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
 
-    margin: 0;
-    padding: 0;
-  }
+  margin: 0;
+  padding: 0;
+}
 
-  svg {
-    cursor: pointer;
-  }
+h2 {
+  font-size: 2.4rem;
+  color: $wood;
 
-  a {
-    text-decoration: none;
-  }
+  margin: 1rem 0 1.4rem;
+}
 
-  button {
-    cursor: pointer;
+h5 {
+  text-transform: uppercase;
+  color: $fountain;
+}
 
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 500;
-    
-    padding: 1em 1.4em;
-    border-radius: 4px;
-    border: none;
-    
-    &.lagoon {
-      background-color: $lagoon;
-      color: $white;
+a {
+  text-decoration: none;
+}
 
-      border: 1px solid transparent;
+svg {
+  cursor: pointer;
+}
 
-      transition: background-color .2s linear;
+span.marked {
+  color: $lagoon;
+  background-color: $mark;
 
-      &:hover {
+  padding: 0 .6rem;
+  border-radius: 4px;
+}
+
+p.paragraph {
+  line-height: 1.4rem;
+  font-size: .8rem;
+
+  color: $silver;
+
+  margin-bottom: 2.6rem;
+}
+
+button {
+  cursor: pointer;
+
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+
+  padding: 1em 1.4em;
+  border-radius: 2px;
+  border: none;
+
+  &.lagoon {
+    background-color: $lagoon;
+    color: $white;
+
+    border: 1px solid transparent;
+
+    transition: background-color 0.2s linear;
+
+    &:hover {
       background-color: $surfe;
-      }
-    }
-
-    &.border {
-      background-color: transparent;
-      color: $white;
-
-      border: 1px solid $lagoon;
-
-      transition: background-color .2s linear;
-
-      &:hover {
-      background-color: $lagoon;
-      }
     }
   }
 
+  &.border {
+    background-color: transparent;
+    color: $white;
+
+    border: 1px solid $lagoon;
+
+    transition: background-color 0.2s linear;
+
+    &:hover {
+      background-color: $lagoon;
+    }
+  }
+}
 </style>
